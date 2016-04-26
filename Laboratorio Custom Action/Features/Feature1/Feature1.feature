61f744cf149d0431cf300fd764eaa27f7fe21bcb
alvaro.ramiro <alraled@hotmail.es>
2016-04-26 13:39:15 +0000

    sharepoint

---
+++ Laboratorio Custom Action/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="81c87b4d-1e86-44ce-b83d-99414cba0a0f" description="SharePoint Add-in Feature" featureId="81c87b4d-1e86-44ce-b83d-99414cba0a0f" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Laboratorio Custom Action Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="c7f29cdf-844b-4f7a-a7b5-c066b01573ff" />
+    <projectItemReference itemId="782729c2-dc60-427f-a782-126cbf951ffc" />
+    <projectItemReference itemId="59dc8a85-1e95-41ca-903d-59c6bb089f91" />
+    <projectItemReference itemId="c2d6112b-d3b0-48b7-8110-9b97759d686f" />
+  </projectItems>
+</feature>
---
+++ Laboratorio Custom Action/Scripts/App.js
@@ -0,0 +1,107 @@
+﻿'use strict';
+
+var context;
+var web;
+var user;
+var currentItem;
+var hostweburl;
+var appweburl;
+var title;
+var list;
+
+
+$(document).ready(function() {
+    appweburl = GetAppSiteUrl();
+    hostweburl = GetHostSiteUrl();
+
+    var scriptbase = hostweburl + "/_layouts/15/";
+    $.getScript(scriptbase + "SP.RequestExecutor.js");
+
+    var clientContext = new SP.ClientContext.get_current();
+    var parentCtx = new SP.AppContextSite(clientContext, hostweburl);
+    var web = parentCtx.get_web();
+    clientContext.load(web);
+    var listId = GetListId();
+    list = web.get_lists().getById(listId);
+    clientContext.load(list);
+    var itemId = GetItemId();
+    currentItem = list.getItemById(itemId);
+    clientContext.load(currentItem);
+
+    clientContext.executeQueryAsync(onlistLoadSucceeded, onRequestFailed);
+
+});
+
+function onListLoadSucceeded() {
+    title = currentItem.get_fieldValues().Name;
+    getSearchResult(title);
+}
+
+function onRequestFailed(sender, args) {
+    alert("Error:" + args.get_message());
+}
+
+function DoSearch() {
+    var query = $('#txtsearch').val();
+    getSearchResult(query);
+}
+
+function getSearchResult(queryText) {
+    $("#search-title").text("buscar resultados para [" + queryText + "]");
+
+    var searchurl = appweburl + "/_api/search/query?querytext=" + queryText + "&trimduplicates=false";
+    var executor = new SP.RequestExecutor(appweburl);
+    executor.executeAsync({
+        url: searchurl,
+        method: "GET",
+        headers: { "Accept": "application/json; odata=verbose" },
+        success: onGetSearchResultsSuccess,
+        error: onGetSearchResultsFail
+    });
+}
+
+function onGetSearchResultsSuccess(data) {
+    var jsonObject = JSON.parse(data.body);
+    var results = jsonObject.d.query.PrimaryQueryResult.RelevantResults.Table.Rows.reuslts;
+    if (results.length == 0) {
+        $("#search-results").text("No se han encontrado items");
+    } else {
+        var searchResultsHtml = "";
+        $.each(results, function(index, result) {
+            searchResultsHtml += "<a target='_blank' href=" + results.Cells.results[6].Value + "'>" + result.Cells.reuslts[6].Value + "</a></br>";
+
+        });
+
+        $("#search-results").html(searchResultsHtml);
+    }
+}
+
+function onGetSearchResultsFail(data, errorCode, errorMessage) {
+    $("#search-results").text("Ha ocurrido un error durante la búsqueda - " + errorMessage);
+}
+
+function getQueryStringParameter(paramToRetrieve) {
+    var params = document.URL.split("?")[1].split("&");
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == paramToRetrieve) {
+            return singleParam[1];
+        }
+    }
+}
+
+function GetListId() {
+    return decodeURIComponent(getQueryStringParameter("ListID"));
+}
+
+function GetHostSiteUrl() {
+    return decodeURIComponent(getQueryStringParameter("SPHostUrl"));
+}
+
+function GetAppSiteUrl() {
+    return decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
+}
+
+function GetItemId() {
+    return getQueryStringParameter("ItemID");
+}
